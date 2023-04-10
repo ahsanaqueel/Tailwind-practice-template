760e1fbab98cd9ebf0d1668757ea2338c2332fc7
ahsan aqueel <ahsansunny08@gmail.com>
2023-04-10 14:34:09 +0500

    typewriter added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Typed from 'react-typed';
+import Typewriter from 'typewriter-effect';
+// import Typed from 'react-typed';
 
 const Hero = () => {
   return (
@@ -8,12 +9,21 @@ const Hero = () => {
             <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
             <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
             <div className='flex justify-center items-center'>
-                <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible financing for</p>
-                <Typed className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
+                <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible financing for
+                {/* <Typed className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'
                  strings={['BTB','BTC','SASS']} 
                  typeSpeed={120} 
                  backSpeed={140} 
-                 loop/>
+                 loop/> */}
+                { <Typewriter
+                   options={{
+                  strings: ['BTB', 'BTC','SASS'],
+                  autoStart: true,
+                  loop: true,
+                  
+  }}
+/>}
+</p>
             </div>
             <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB BTC & SASS platforms</p>
             <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
